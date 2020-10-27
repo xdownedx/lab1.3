a1034988ab74d4b7d6ed71a31d88a60043f0047b
Max Palehin <ars_kit@icloud.com>
2020-10-27 15:58:43 +0300

    Initial Commit

---
+++ lab1_3test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab1_3test
+//
+//  Created by Максим Палёхин on 27.10.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
